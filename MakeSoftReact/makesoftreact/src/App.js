3cf991a7578378cae2bfa6472ab6da3fa7ca4299
Firas <firas.haddad.h@gmail.com>
2024-09-24 19:48:22 -0400

    Updating home page of the website

--- MakeSoftReact/makesoftreact/src/App.js
+++ MakeSoftReact/makesoftreact/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Signup from './SignUp'; // Import the Signup component from the SignUp.js file
 import Signin from './SignIn';
 import conco from './Conco.png';
+import './SignUp.css';
 import conco_library from './Conco-library.jpg';
 import conco_picture from './Conco-picture.webp';
 
@@ -37,13 +38,50 @@ function App() {
           </MenuItem>
         </MenuMenu>
       </Menu>
-      <header className="App-header"style={{
-      backgroundImage: `url(${conco_picture})`,filter: 'blur(0.5px)', position: 'absolute', margin: '0', backgroundRepeat: 'no-repeat', display: 'flex' , height: '130px', width: '100%', backgroundPosition: 'top left',backgroundSize: 'contain'}}>
+      <header>
+      
         <h1> </h1>
       </header>
       {/* Render the current component based on state */}
       {currentComponent === 'signup' && <Signup />}
-      {currentComponent === 'home' && <div>Welcome to the Home Page</div> }
+      {currentComponent === 'home' && 
+      <div style={{
+        backgroundImage: `url(${conco_library})`,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh', // Full height of viewport
+        width: '100%', // Full width of viewport
+        backgroundSize: 'cover', // Ensure image covers entire background
+        backgroundPosition: 'center', // Center the image
+        backgroundRepeat: 'no-repeat', // Prevent repetition of the image
+      }}>
+      
+      
+        <div className="content2">
+        <span style={{fontSize: '30px'}}>What is MakeSoft Peer Evaluation</span><br></br><br></br>
+        <span style={{color: 'rgb(149, 38, 38)'}}>MakeSoft Peer Assessment System </span> is designed for university team projects, allowing students to evaluate their teammates based on, cooperation, conceptual contribution, practical contribution, and work ethic. The system is meant to promote accountability and provide feedback to both students and instructors about individual contributions. Instructors can manage teams, monitor performance, and export results for grading purposes.
+        <br></br><br></br>
+      
+         </div>
+         <div className="content2"><span style={{fontSize: '30px'}}>How to use it</span> <br></br><br></br>
+        For <span style={{color: 'rgb(149, 38, 38)'}}>Teachers </span>create an account and sign in. Once you log in, you'll have access to all students that signed up to the website and you'll be able to 
+         assign teams by adding the students from the same section. After that, you can modify teams, remove and add students.
+        If you are a <span style={{color: 'rgb(149, 38, 38)'}}>Student </span>, create an account as well and sign in. Then you'll see all the teams available and you can join a team that didn't reach its maximum capacity.
+
+         </div>
+
+         <div className="content2"><span style={{fontSize: '30px'}}>Contact US</span> <br></br><br></br>
+         We'll be glad to answer any inquiries at MakeSoft@gmail.com <br></br>
+         Phone number: +1 514 534 6356
+         </div>
+        
+        
+        
+        </div>
+        
+         }
       {currentComponent === 'signin' && <Signin />}
     </div>
   );
